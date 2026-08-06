931fe57ea2bec84d173ce719e80efc2b7578a744
unknown <dudupanont@gmail.com>
2026-08-06 08:53:26 -0300

    Implementacao Usuario e comparacao(equal)

--- src/br/com/streaming/model/Usuario.java
+++ src/br/com/streaming/model/Usuario.java
@@ -15,6 +15,7 @@ public class Usuario {
         this.email = email;
         this.nome = nome;
         this.ativo = true;
+        contadorUsuarios++;
         // TODO: Instanciar a lista de vídeos e incrementar o contador
     }
 
@@ -22,4 +23,66 @@ public class Usuario {
         // TODO: Adicionar o vídeo na lista de histórico
     }
     // TODO: Gerar Getters, Setters, equals(), hashCode() e toString()
+
+
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario [email=" + email + ", nome=" + nome + ", ativo=" + ativo + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((email == null) ? 0 : email.hashCode());
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        result = prime * result + (ativo ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Usuario other = (Usuario) obj;
+        if (email == null) {
+            if (other.email != null)
+                return false;
+        } else if (!email.equals(other.email))
+            return false;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        if (ativo != other.ativo)
+            return false;
+        return true;
+    }
+
+   public static final String NOME_PLATAFORMA = "JavaFlix";
+   public static int contadorUsuarios = 0;
+   
+    
 }
